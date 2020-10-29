dac0e700d4a7e2a5b4e210339c672fbc83b40005
Frances Murphy <framurph@publicisgroupe.net>
2020-10-29 17:56:59 -0400

    created shop mode component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.scss';
+import ShopMode from './features/shopMode/ShopMode';
 
 function App() {
   return (
     <div className="app">
       {/* Everything goes in here */}
+      <ShopMode />
     </div>
   );
 }
--- src/features/shopMode/ShopMode.js
+++ src/features/shopMode/ShopMode.js
@@ -1,32 +1,37 @@
 import React from 'react';
-//  import '@shopify/polaris/dist/styles.css';
-//  import enTranslations from '@shopify/polaris/locales/en.json';
 import { Button } from '@shopify/polaris';
+import './ShopMode.css';
+import { FaAngleRight } from 'react-icons/fa';
 
 const ShopMode = () => {
-  //  assume our container will have the state varuiable for shopmode and pass it down as a prop
+  //  assume our container will have the state variable for shopmode and
+  // method to update and we just pass our value into prop method
   let shopMode = 'HI';
 
   const updateShopMode = (value) => {
     shopMode = value;
     console.log(shopMode);
-    return value;
   };
 
   return (
-    <div>
+    <div className="shop-mode-container">
       <h1>Select your shopping experience</h1>
       <h2>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
         incididunt ut labore et dolore magna aliqua.
       </h2>
-      <button type="button" onClick={() => updateShopMode('quick')}>
-        Quick Shop
-      </button>
-      <button type="button" onClick={() => updateShopMode('safe')}>
-        Avoid Crowds
-      </button>
-      <Button outline>Add product</Button>
+      <Button outline onClick={() => updateShopMode('quick')}>
+        <h3>Quick Shop</h3>
+        <div className="button-text">
+          Shows the most efficient path for picking up your items
+        </div>
+        <FaAngleRight className="button-icon" />
+      </Button>
+      <Button outline onClick={() => updateShopMode('safe')}>
+        <h3>Avoid Crowds</h3>
+        <div className="button-text">Shows the path with the least amount of foot traffic</div>
+        <FaAngleRight className="button-icon" />
+      </Button>
     </div>
   );
 };
