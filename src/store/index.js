9500c338ac4457be53fc6e7265a42941ea1da48a
Tom Mineo <txm3278@rit.edu>
2020-10-29 15:10:29 -0400

    formatting

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { combineReducers } from 'redux';
 
-const reducer = combineReducers({
-});
+const reducer = combineReducers({});
 const store = configureStore({
   reducer,
 });
