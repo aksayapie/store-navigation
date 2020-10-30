0e438b767923a818073f26a57cbb15fb38b0eb6d
Frances Murphy <framurph@publicisgroupe.net>
2020-10-30 10:54:09 -0400

    made screen buttons their own compoonent

--- src/features/shopMode/ShopMode.js
+++ src/features/shopMode/ShopMode.js
@@ -1,39 +1,25 @@
 import React from 'react';
-import { Button } from '@shopify/polaris';
 import './ShopMode.scss';
-import { FaAngleRight } from 'react-icons/fa';
+import ShopModeButton from './ShopModeButton';
 
-const ShopMode = () => {
-  //  assume our container will have the state variable for shopmode and
-  // method to update and we just pass our value into prop method
-  let shopMode = 'HI';
-
-  const updateShopMode = (value) => {
-    shopMode = value;
-    console.log(shopMode);
-  };
-
-  return (
-    <div className="shop-mode-container">
-      <h1 className="bold">Select your shopping experience</h1>
-      <h2>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-        incididunt ut labore et dolore magna aliqua.
-      </h2>
-      <Button outline onClick={() => updateShopMode('quick')}>
-        <h3>Quick Shop</h3>
-        <div className="button-text">
-          Shows the most efficient path for picking up your items
-        </div>
-        <FaAngleRight className="button-icon" />
-      </Button>
-      <Button outline onClick={() => updateShopMode('safe')}>
-        <h3>Avoid Crowds</h3>
-        <div className="button-text">Shows the path with the least amount of foot traffic</div>
-        <FaAngleRight className="button-icon" />
-      </Button>
-    </div>
-  );
-};
+const ShopMode = () => (
+  <div className="shop-mode-container">
+    <h1 className="bold">Select your shopping experience</h1>
+    <h2>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+      incididunt ut labore et dolore magna aliqua.
+    </h2>
+    <ShopModeButton
+      shopModeProp="quick"
+      headerProp="Quick Shop"
+      bodyProp="Shows the most efficient path for picking up your items"
+    />
+    <ShopModeButton
+      shopModeProp="safe"
+      headerProp="Avoid Crowds"
+      bodyProp="Shows the path with the least amount of foot traffic"
+    />
+  </div>
+);
 
 export default ShopMode;
---
+++ src/features/shopMode/ShopModeButton.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Button } from '@shopify/polaris';
+import './ShopMode.scss';
+import { FaAngleRight } from 'react-icons/fa';
+import PropTypes from 'prop-types';
+
+//  assume our container will have the state variable for shopmode and
+// method to update and we just pass our value into prop method
+
+function ShopModeButton({ shopModeProp, headerProp, bodyProp }) {
+  const updateShopMode = (value) => {
+    console.log(value);
+  };
+
+  return (
+    <div>
+      <Button outline onClick={() => updateShopMode(shopModeProp)}>
+        <h3>{headerProp}</h3>
+        <div className="button-text">
+          {bodyProp}
+        </div>
+        <FaAngleRight className="button-icon" />
+      </Button>
+    </div>
+  );
+}
+
+ShopModeButton.propTypes = {
+  shopModeProp: PropTypes.string.isRequired,
+  headerProp: PropTypes.string.isRequired,
+  bodyProp: PropTypes.string.isRequired,
+};
+
+export default ShopModeButton;
