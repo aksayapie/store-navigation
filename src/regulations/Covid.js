2fef96c384fe779158e5305c3234d0e446c249ef
Aksaya Manickandan <aksmanic1@publicisgroupe.net>
2020-10-30 10:55:36 -0400

    Fixed css for shopping button and removed header buttons

--- src/regulations/Covid.js
+++ src/regulations/Covid.js
@@ -1,34 +1,28 @@
 import React from 'react';
-import { Button } from '@shopify/polaris';
-import { FaArrowLeft, FaTimes } from 'react-icons/fa';
+import { Button, ButtonGroup } from '@shopify/polaris';
 import './Covid.scss';
 
 function Covid() {
   return (
-    <div>
-      <div className="header">
-        <Button>
-          <FaArrowLeft className="back-button" />
-        </Button>
-        <Button>
-          <FaTimes className="exit-button" />
-        </Button>
+    <div className="content">
+      <div className="main-text">COVID-19 Policies</div>
+      <div className="sub-text">
+        To protect our members and employees, all Costco members and guests
+        must wear a face covering that covers their mouth and nose at all
+        times while at Costco.
       </div>
-
-      <div className="content">
-        <div className="main-text">COVID-19 Policies</div>
-        <div className="sub-text">
-          To protect our members and employees, all Costco members and guests
-          must wear a face covering that covers their mouth and nose at all
-          times while at Costco.
-        </div>
-        <Button>
-          <div className="back">Back</div>
-        </Button>
-        <Button>
-          <div className="start">Start Shopping</div>
-        </Button>
-      </div>
+      {/* <Button>
+        <div className="back">Back</div>
+      </Button>
+      <Button>
+        <div className="start">Start Shopping</div>
+      </Button> */}
+      <ButtonGroup>
+        <Button>Back</Button>
+        <div className="shopping"><Button>Shopping</Button></div>
+      </ButtonGroup>
+      {/* <Button>Back</Button>
+      <div className="shopping"><Button>Shopping</Button></div> */}
     </div>
   );
 }
