2ca33d0a374078d23be80f333287677e5eda93eb
Ulzii <uxb6317@rit.edu>
2020-10-30 14:38:04 -0400

    made sure google runs its scripts before we use the map

--- src/features/map/Map.js
+++ src/features/map/Map.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {
-  GoogleMap, LoadScript, GroundOverlay, Polyline,
+  GoogleMap, GroundOverlay, Polyline, Marker, useJsApiLoader,
 } from '@react-google-maps/api';
 import { useSelector } from 'react-redux';
 
@@ -8,14 +8,14 @@ import costco from '../../assets/costco.png';
 import { selectPath } from './mapSlice';
 import { BOUNDS, CENTER } from '../../constants';
 import './map.scss';
+import Bounty from '../../assets/bounty.png';
 
 // path polyline options
 const options = {
-  strokeColor: '#FF0000',
+  strokeColor: '#E31837',
   strokeOpacity: 0.8,
   strokeWeight: 1,
-  fillColor: '#FF0000',
-  fillOpacity: 0.35,
+  fillColor: '#E31837',
   clickable: false,
   draggable: false,
   editable: false,
@@ -26,41 +26,58 @@ const options = {
 
 const mapOptions = {
   disableDefaultUI: true,
+  maxZoom: 20,
+  minZoom: 19,
 };
 
 const Map = () => {
+  // load the google map javascript scripts
+  const { isLoaded } = useJsApiLoader({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_API_KEY,
+  });
   const path = useSelector(selectPath);
 
   return (
     <>
-      <LoadScript
-        googleMapsApiKey={process.env.REACT_APP_GOOGLE_API_KEY}
-      >
+      {isLoaded
+      && (
         <GoogleMap
-          id="map-overlay"
-          mapContainerStyle={{
-            width: '100%',
-            height: '100%',
-          }}
+          id="costco-map"
+          mapContainerClassName="mapStyles"
           zoom={19}
           center={CENTER}
           options={mapOptions}
         >
+          {/* Overlay an image of Costco within the bounds */}
           <GroundOverlay
-            key="url"
             url={costco}
             bounds={BOUNDS}
           />
           {
+            // render polylines if there are paths to render
             path.length > 0 && (
-            <Polyline
-              path={path}
-              options={options}
-            />
+            <>
+              <Polyline
+                path={path}
+                options={options}
+              />
+              {
+              // render item positions
+              path.map((position) => (
+                <Marker
+                  key={position.lat}
+                  icon={
+                  { url: Bounty, scaledSize: new window.google.maps.Size(40, 40) }
+                }
+                  position={position}
+                />
+              ))
+            }
+            </>
             )
         }
         </GoogleMap>
-      </LoadScript>
+      )}
     </>
   );
 };
