3ca87183ed80385ce2a52efba0f5791f3893466f
Frances Murphy <framurph@publicisgroupe.net>
2020-11-02 17:30:31 -0500

    created nav bar

--- src/App.js
+++ src/App.js
@@ -7,29 +7,34 @@ import FlowContainer from './features/flowContainer/FlowContainer';
 import MapPage from './features/map/MapPage';
 import ScanPage from './features/scan/ScanPage';
 import FrontPageShoppingList from './features/ShoppingList/FrontPageShoppingList';
+import MainNavBar from './components/mainNavBar/MainNavBar';
 
 function App() {
   return (
     <AppProvider i18n={enTranslations}>
       <div className="app" id="app">
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-        <Switch>
-          {/* Wrap your page component in a <Route> component and it will display
-              when the url matches the path. */}
-          <Route path="/map">
-            <MapPage />
-          </Route>
-          <Route path="/welcome">
-            <FlowContainer />
-          </Route>
-          <Route path="/scan">
-            <ScanPage />
-          </Route>
-          <Route path="/">
-            <FrontPageShoppingList />
-          </Route>
-        </Switch>
+        <MainNavBar />
+        {/* using margin top to give nav bar enough space */}
+        <div style={{ marginTop: 16 }}>
+          {/* A <Switch> looks through its children <Route>s and
+          renders the first one that matches the current URL. */}
+          <Switch>
+            {/* Wrap your page component in a <Route> component and it will display
+                when the url matches the path. */}
+            <Route path="/map">
+              <MapPage />
+            </Route>
+            <Route path="/welcome">
+              <FlowContainer />
+            </Route>
+            <Route path="/scan">
+              <ScanPage />
+            </Route>
+            <Route path="/">
+              <FrontPageShoppingList />
+            </Route>
+          </Switch>
+        </div>
       </div>
     </AppProvider>
   );
