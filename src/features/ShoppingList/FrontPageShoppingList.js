500cc8006ffc617c1c01fdea7fa2f029b93a5e98
McKenzie Murphy <framurph@publicisgroupe.net>
2020-11-03 16:25:59 -0500

    a lot of styling changes

--- src/features/ShoppingList/FrontPageShoppingList.js
+++ src/features/ShoppingList/FrontPageShoppingList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Page } from '@shopify/polaris';
+// import { Page } from '@shopify/polaris';
 import ShoppingList, { ShoppingListTitle } from './ShoppingList';
 import ShoppingListHeader from './ShoppingListHeader';
 import ShoppingListBottomButtons from './ShoppingListBottomButtons';
@@ -10,14 +10,18 @@ export default function FrontPageShoppingList() {
   return (
     <div className="shopping-list-container">
       <ListPageTopBar />
-      <Page title="My Account">
-        <ShoppingListHeader />
-        <br />
+      {/* <Page title="My Account"> */}
+      <ShoppingListHeader />
+      <br />
+      <div className="list-title">
         <ShoppingListTitle />
-        <br />
+      </div>
+      <br />
+      <div className="shopping-item-container">
         <ShoppingList />
-        <ShoppingListBottomButtons />
-      </Page>
+      </div>
+      <ShoppingListBottomButtons />
+      {/* </Page> */}
     </div>
   );
 }
