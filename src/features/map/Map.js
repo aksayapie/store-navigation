b1e273d2e9a449819c6b256b025d96df93fae2ed
Ulzii <uxb6317@rit.edu>
2020-11-04 11:50:32 -0500

    added propTypes, renders shelf polygons from redux

--- src/features/map/Map.js
+++ src/features/map/Map.js
@@ -1,18 +1,12 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable react/prop-types */
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import {
   GoogleMap, Polyline, Marker, useJsApiLoader, Polygon,
 } from '@react-google-maps/api';
 import PropTypes from 'prop-types';
 
-import {
-  CENTER, TRANSLATE_SHELF, EXPAND_SHELF,
-} from '../../constants';
+import { CENTER } from '../../constants';
 import './map.scss';
 import ListPopup from '../../components/listPopup/listPopup';
-
-import testData from '../../data3.json';
 
 // TODO: customize lines to look better
 // path polyline options
@@ -31,75 +25,13 @@ const mapOptions = {
   disableDefaultUI: true,
   maxZoom: 20,
   minZoom: 18,
-  tile: 45,
 };
 
-const Map = ({ path, shoppingList }) => {
-  const [shelves, setShelves] = useState([]);
+const Map = ({ path, shelfPolygons, shoppingList }) => {
   // load the google map javascript scripts
   const { isLoaded } = useJsApiLoader({
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_API_KEY,
   });
-
-  const data = {
-    name: 'citrus fruit',
-    price: 4.39,
-    inStock: false,
-    lat: 40.655664916666666,
-    lng: -74.00979675402047,
-    aisleNumber: 1,
-    shelfNumber: 0,
-    UPC: '783048567123',
-    imageURL: '#',
-  };
-
-  useEffect(() => {
-    const extractPath = (aislePoints) => aislePoints.filter((point, index) => {
-      if (aislePoints.length - 1 === index || index === 0) return true;
-      return false;
-    });
-
-    const shiftPath = (aislePaths) => aislePaths.map((cur) => ({
-      lat: cur.lat + TRANSLATE_SHELF.lat,
-      lng: cur.lng + TRANSLATE_SHELF.lng,
-    }));
-
-    const scaleShelf = (shelfPaths) => [...shelfPaths, {
-      lat: shelfPaths[1].lat + EXPAND_SHELF.lat,
-      lng: shelfPaths[1].lng + EXPAND_SHELF.lng,
-    },
-    {
-      lat: shelfPaths[0].lat + EXPAND_SHELF.lat,
-      lng: shelfPaths[0].lng + EXPAND_SHELF.lng,
-    }];
-
-    const divideAisle = (aisle) => {
-      const one = aisle.slice(0, 5);
-      const two = aisle.slice(5);
-
-      return [scaleShelf(shiftPath(extractPath(one))), scaleShelf(shiftPath(extractPath(two)))];
-    };
-
-    let aisleNumber = 1;
-    let aisleArr = [];
-
-    const sortedShelves = testData.reduce((acc, curr) => {
-      if (curr.aisleNumber === aisleNumber) {
-        aisleArr.push(curr);
-      } else {
-        const [one, two] = divideAisle(aisleArr);
-        acc.push(one);
-        acc.push(two);
-        aisleArr = [];
-        aisleArr.push(curr);
-        aisleNumber += 1;
-      }
-
-      return acc;
-    }, []);
-
-    setShelves(sortedShelves);
-  }, []);
 
   return (
     <>
@@ -113,9 +45,10 @@ const Map = ({ path, shoppingList }) => {
         center={path?.length > 0 ? path[0] : CENTER}
         options={mapOptions}
       >
-        {/* {
-            // render lines if there is a path to render
-            path && path.length > 0 && (
+        {shelfPolygons.length > 0 && shelfPolygons.map((shelf) => <Polygon path={shelf} />)}
+        {
+          // render lines if there is a path to render
+          path && path.length > 0 && (
             <Polyline
               path={path.map((point) => ({
                 lat: point.lat,
@@ -123,35 +56,56 @@ const Map = ({ path, shoppingList }) => {
               }))}
               options={pathLineOptions}
             />
-            )
-          } */}
-        {
-        shelves && shelves.map((shelf) => <Polygon path={shelf} />)
-      }
+          )
+        }
 
         {
-            testData.map((itemData) => (
-              <Marker position={{ lat: itemData.lat, lng: itemData.lng }} />
-            ))
+          shoppingList && shoppingList.length > 0
+            && shoppingList.map((item) => {
+              if (item.inCart) return null;
+              return (
+                <Marker
+                  key={item.upc}
+                  position={{ lat: item.lat, lng: item.lng }}
+                />
+              );
+            })
           }
-        {/* {
-            shoppingList && shoppingList.length > 0
-              && shoppingList.map((item) => {
-                if (item.inCart) return null;
-                return (
-                  <Marker
-                    key={item.upc}
-                    icon={{ url: PaperTowel, scaledSize: new window.google.maps.Size(25, 25) }}
-                    position={{ lat: item.lat, lng: item.lng }}
-                  />
-                );
-              })
-          } */}
       </GoogleMap>
       )}
       <ListPopup />
     </>
   );
+};
+
+Map.propTypes = {
+  path: PropTypes.arrayOf(PropTypes.shape({
+    upc: PropTypes.string.isRequired,
+    lat: PropTypes.number.isRequired,
+    lng: PropTypes.number.isRequired,
+  })),
+  shelfPolygons: PropTypes.arrayOf(PropTypes.arrayOf(PropTypes.shape(
+    {
+      lat: PropTypes.number.isRequired,
+      lng: PropTypes.number.isRequired,
+    },
+  ))).isRequired,
+  shoppingList: PropTypes.arrayOf(PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+    stock: PropTypes.bool.isRequired,
+    lat: PropTypes.number.isRequired,
+    lng: PropTypes.number.isRequired,
+    aisle: PropTypes.number.isRequired,
+    upc: PropTypes.string.isRequired,
+    imageURL: PropTypes.string,
+    inCart: PropTypes.bool.isRequired,
+  })),
+};
+
+Map.defaultProps = {
+  path: [],
+  shoppingList: [],
 };
 
 export default React.memo(Map);
