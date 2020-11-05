e61a3c324a59f7448f2f28edb4944b0279d37cdc
Tom Mineo <txm3278@rit.edu>
2020-11-05 11:56:34 -0500

    changed scan to use 'best' camera

--- src/features/scan/ScanPage.js
+++ src/features/scan/ScanPage.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import { BrowserMultiFormatReader, BrowserCodeReader } from '@zxing/browser';
+import { BrowserMultiFormatReader } from '@zxing/browser';
 import { Button, Icon, ButtonGroup } from '@shopify/polaris';
 import { MobileCancelMajor } from '@shopify/polaris-icons';
 import Modal from 'react-modal';
@@ -9,7 +9,6 @@ import { addItemToCart } from '../ShoppingList/shoppingListSlice';
 import './ScanPage.scss';
 
 const ScanPage = () => {
-  const [code, setCode] = useState(null);
   const [control, setControls] = useState(null);
   const [modalIsOpen, setIsOpen] = useState(false);
   const [scannedItem, setItem] = useState(null);
@@ -20,24 +19,19 @@ const ScanPage = () => {
   useEffect(() => {
     const scanBarcode = async () => {
       const codeReader = new BrowserMultiFormatReader();
-      const videoInputDevices = await BrowserCodeReader.listVideoInputDevices();
-
-      // choose your media device (webcam, frontal camera, back camera, etc.)
-      const selectedDeviceId = videoInputDevices[0].deviceId;
 
       const previewElem = document.querySelector('video');
 
       // you can use the controls to stop() the scan or switchTorch() if available
       setControls(
         await codeReader.decodeFromVideoDevice(
-          selectedDeviceId,
+          undefined,
           previewElem,
           (result) => {
             // use the result and error values to choose your actions
             // you can also use controls API in this scope like the controls
             // returned from the method.
             if (result) {
-              setCode(result.text.substring(1));
               const foundItem = items.find((item) => item.UPC === result.text.substring(1));
               if (foundItem) {
                 setItem(foundItem);
@@ -71,7 +65,6 @@ const ScanPage = () => {
         </Link>
         <h1>Scanner</h1>
         <p>Center the viewer over a barcode to add an item to your list</p>
-        <p>{code}</p>
       </div>
       <Modal
         isOpen={modalIsOpen}
