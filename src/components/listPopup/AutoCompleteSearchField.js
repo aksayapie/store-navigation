c39812f140fc5a076ab80dfac99792c2b5956bcc
Frances Murphy <framurph@publicisgroupe.net>
2020-11-06 14:20:04 +0000

    Update AutoCompleteSearchField.js

--- src/components/listPopup/AutoCompleteSearchField.js
+++ src/components/listPopup/AutoCompleteSearchField.js
@@ -46,7 +46,7 @@ function AutoCompleteSearchField({ handleCloseProp }) {
   };
 
   return (
-    <div className="search-container" style={{ height: '500px' }}>
+    <div className="search-container" style={{ height: '100%' }}>
       <div className="search-box">
         <TextField
           label="itemSearchTextField"
